d9992513091b2048b5e54410b7e3e9106cdaf305
jenkinsnetstr <mnomkar2015@gmail.com>
2015-05-29 17:27:39 +0530

    Added hello.py script.

---
+++ hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def greetings(string):
+    if string == "hello":
+        print "**Greetings from hello Function**"
+        print 
+        print "hello :)"
+        print "Welcome to world of python"
+        print
+        print "**End of Greetings from hello Function**"
+    else:
+        print 
+        print "hi, thanks for greetings. Welcome to python."
+        print
+
+if __name__ == '__main__':
+    greetings("hello")
